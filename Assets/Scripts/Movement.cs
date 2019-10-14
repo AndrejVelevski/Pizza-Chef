7fe8119caf66f2c46dee63d56dfdf80de72516a4
Andrej <andrej_velevski@hotmail.com>
2019-10-14 02:59:24 +0200

    Initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 2;
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+
+        Vector3 forwards = transform.forward * vertical;
+        Vector3 sideways = transform.right * horizontal;
+        Vector3 direction = (forwards + sideways).normalized;
+
+        Vector3 velocity = Camera.main.transform.TransformDirection(direction * moveSpeed * Time.deltaTime);
+        velocity.y = 0;
+
+        controller.Move(velocity);
+    }
+}
