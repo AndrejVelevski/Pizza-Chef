17c34d52cc3483ef1219e41b01e8ae8ec627bc8e
mVucevski <placeformovies@gmail.com>
2019-10-26 22:52:24 +0200

    Add respawn objects

--- Assets/Scripts/PizzaScript.cs
+++ Assets/Scripts/PizzaScript.cs
@@ -13,13 +13,18 @@ public class PizzaScript : MonoBehaviour
 
     System.Random random = new System.Random();
 
+    bool[] takenIng;
+
     void Start()
     {
         bakeTime = random.Next(10,21);
+        takenIng = new bool[16];    
     }
 
     private void Update()
     {
+        //if (respawnIng.Length <= 0) initRespawn();
+
         if (!isDone && isBaking)
         {
             bakeTime -= Time.deltaTime;
@@ -38,22 +43,22 @@ public class PizzaScript : MonoBehaviour
         {
             switch (collision.gameObject.tag)
             {
-                case "Apple": ingredients += (int)Food.Apple; Destroy(collision.gameObject); break;
-                case "Avocado": ingredients += (int)Food.Avocado; Destroy(collision.gameObject); break;
-                case "Banana": ingredients += (int)Food.Banana; Destroy(collision.gameObject); break;
-                case "Broccoli": ingredients += (int)Food.Broccoli; Destroy(collision.gameObject); break;
-                case "Carrot": ingredients += (int)Food.Carrot; Destroy(collision.gameObject); break;
-                case "Cheese": ingredients += (int)Food.Cheese; Destroy(collision.gameObject); break;
-                case "Cucumber": ingredients += (int)Food.Cucumber; Destroy(collision.gameObject); break;
-                case "Leek": ingredients += (int)Food.Leek; Destroy(collision.gameObject); break;
-                case "Lemon": ingredients += (int)Food.Lemon; Destroy(collision.gameObject); break;
-                case "Meat": ingredients += (int)Food.Meat; Destroy(collision.gameObject); break;
-                case "Mozzarella": ingredients += (int)Food.Mozzarella; Destroy(collision.gameObject); break;
-                case "Mushroom": ingredients += (int)Food.Mushroom; Destroy(collision.gameObject); break;
-                case "Onion": ingredients += (int)Food.Onion; Destroy(collision.gameObject); break;
-                case "Pear": ingredients += (int)Food.Pear; Destroy(collision.gameObject); break;
-                case "Pepperoni": ingredients += (int)Food.Pepperoni; Destroy(collision.gameObject); break;
-                case "Tomato": ingredients += (int)Food.Tomato; Destroy(collision.gameObject); break;
+                case "Apple": ingredients += (int)Food.Apple; collision.gameObject.SetActive(false); takenIng[0] = true; break;
+                case "Avocado": ingredients += (int)Food.Avocado; collision.gameObject.SetActive(false); takenIng[1] = true; break;
+                case "Banana": ingredients += (int)Food.Banana; collision.gameObject.SetActive(false); takenIng[2] = true; break;
+                case "Broccoli": ingredients += (int)Food.Broccoli; collision.gameObject.SetActive(false); takenIng[3] = true; break;
+                case "Carrot": ingredients += (int)Food.Carrot; collision.gameObject.SetActive(false); takenIng[4] = true; break;
+                case "Cheese": ingredients += (int)Food.Cheese; collision.gameObject.SetActive(false); takenIng[5] = true; break;
+                case "Cucumber": ingredients += (int)Food.Cucumber; collision.gameObject.SetActive(false); takenIng[6] = true; break;
+                case "Leek": ingredients += (int)Food.Leek; collision.gameObject.SetActive(false); takenIng[7] = true; break;
+                case "Lemon": ingredients += (int)Food.Lemon; collision.gameObject.SetActive(false); takenIng[8] = true; break;
+                case "Meat": ingredients += (int)Food.Meat; collision.gameObject.SetActive(false); takenIng[9] = true; break;
+                case "Mozzarella": ingredients += (int)Food.Mozzarella; collision.gameObject.SetActive(false); takenIng[10] = true; break;
+                case "Mushroom": ingredients += (int)Food.Mushroom; collision.gameObject.SetActive(false); takenIng[11] = true; break;
+                case "Onion": ingredients += (int)Food.Onion; collision.gameObject.SetActive(false); takenIng[12] = true; break;
+                case "Pear": ingredients += (int)Food.Pear; collision.gameObject.SetActive(false); takenIng[13] = true; break;
+                case "Pepperoni": ingredients += (int)Food.Pepperoni; collision.gameObject.SetActive(false); takenIng[14] = true; break;
+                case "Tomato": ingredients += (int)Food.Tomato; collision.gameObject.SetActive(false); takenIng[15] = true; break;
             }
         }
 
@@ -85,6 +90,8 @@ public class PizzaScript : MonoBehaviour
             }
 
             pizzaRequester.addScore((total - 2*incorrect) * 10 + (isDone?50:-50));
+
+            gameObject.GetComponent<Respawn>().respawn(takenIng);
 
             pizzaRequester.generateNewOrder();
 
---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+
+    public GameObject[] respawnObjects;
+    Vector3[] objectPositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectPositions = new Vector3[respawnObjects.Length];
+        for (int i = 0; i < respawnObjects.Length; i++)
+        {
+            objectPositions[i] = respawnObjects[i].transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void respawn(bool []removedObjects) {
+
+        for (int i = 0; i < removedObjects.Length; i++)
+        {
+            if (removedObjects[i])
+            {
+                //Debug.Log("I: " + i);
+                //Debug.Log("respawnsize: " + respawnIng.Length + " ss " + takenIng.Length);
+                respawnObjects[i].transform.position = objectPositions[i];
+                respawnObjects[i].SetActive(true);
+            }
+        }
+    }
+}
