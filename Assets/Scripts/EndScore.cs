f526edebb11be32fbdb0836b9f32c63082478fde
mVucevski <placeformovies@gmail.com>
2019-10-27 15:47:23 +0100

    Fix

---
+++ Assets/Scripts/EndScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndScore : MonoBehaviour
+{
+	public static int finalScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        finalScore = 0;
+		DontDestroyOnLoad(gameObject);
+    }
+
+}
